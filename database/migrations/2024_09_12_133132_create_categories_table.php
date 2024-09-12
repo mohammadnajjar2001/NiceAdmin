59afd788ca80d5899a2b4233b1db746d743f7815
mohammadnajjar2001 <njar88503@gmail.com>
2024-09-12 17:16:36 +0300

    nullable->description->nullable

--- database/migrations/2024_09_12_133132_create_categories_table.php
+++ database/migrations/2024_09_12_133132_create_categories_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description')->default('empty');
+            $table->text('description')->default('empty')->nullable();
             $table->string('icon')->nullable();
             $table->timestamps();
         });
